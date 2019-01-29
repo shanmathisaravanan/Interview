c9b1c87b94cf3e62ec0709c3db902ba7c4aff46d
shanmathisaravanan <shanmathi291998@gmail.com>
2019-01-29 23:15:37 +0530

    Add files via upload

---
+++ simple1.java
@@ -0,0 +1,18 @@
+
+package guvi;
+
+import java.util.Scanner;
+
+public class simple1 {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        if(n > 0)
+             System.out.println("Positive");
+        else if(n < 0)
+             System.out.println("Negative");
+        else
+             System.out.println("Zero");
+    }
+}
