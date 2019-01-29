92fcfac633894ec029eb70901b1384c9b0fa2111
shanmathisaravanan <shanmathi291998@gmail.com>
2019-01-29 23:21:30 +0530

    Add files via upload

--- Simple2.java
+++ Simple2.java
@@ -6,11 +6,19 @@ import java.util.Scanner;
 public class Simple2 {
         public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        try
+        {
         int n = sc.nextInt();
         
         if(n % 2 == 0)
              System.out.println("Even");
         else 
              System.out.println("Odd");
+        }
+        catch(Exception e)
+        {
+               System.out.println("Invalid");     
+        }
+        
         } 
 }
